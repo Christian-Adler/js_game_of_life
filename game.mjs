aa91b66d71d10ff26f58ee66e43087483a49fe0b
Christian Adler <chris.adler@gmx.de>
2024-06-15 12:03:18 +0200

    initial

---
+++ game.mjs
@@ -0,0 +1,140 @@
+class Game {
+  constructor() {
+    this.livingCells = new Map();
+    this.xMin = -1;
+    this.xMax = 1;
+    this.yMin = -1;
+    this.yMax = 1;
+  }
+
+  addLivingCell(cell) {
+    this.livingCells.set(cell.getKey(), cell);
+  }
+
+  fillRandom() {
+    // while (this.livingCells.size < 20) {
+    //
+    //   const x = Math.floor(Math.random() * 100 - 50);
+    //   const y = Math.floor(Math.random() * 100 - 50);
+    //
+    //   const cell = new Cell(x, y);
+    //   if (!this.livingCells.has(cell.getKey()))
+    //     this.addLivingCell(cell);
+    // }
+
+
+    this.addLivingCell(new Cell(0, 0));
+    this.addLivingCell(new Cell(1, 0));
+    this.addLivingCell(new Cell(2, 0));
+
+    this.addLivingCell(new Cell(10, 10));
+    this.addLivingCell(new Cell(12, 10));
+    this.addLivingCell(new Cell(11, 11));
+    this.addLivingCell(new Cell(12, 11));
+    this.addLivingCell(new Cell(11, 12));
+
+    this.calcMinMax();
+  }
+
+  calcMinMax() {
+    const cells = [...this.livingCells.values()];
+    this.xMin = Math.min(...cells.map(c => c.x));
+    this.xMax = Math.max(...cells.map(c => c.x));
+    this.yMin = Math.min(...cells.map(c => c.y));
+    this.yMax = Math.max(...cells.map(c => c.y));
+  }
+
+  step() {
+    const nextGenCells = [];
+
+    const deadNeighborKeysOfSoFarLivingCells = new Set();
+
+    for (const actCell of this.livingCells.values()) {
+      const livingNeighbors = [];
+      const deadNeighbors = [];
+
+      const neighborKeys = actCell.getNeighborKeys();
+      for (const neighborKey of neighborKeys) {
+        if (this.livingCells.has(neighborKey))
+          livingNeighbors.push(neighborKey);
+        else
+          deadNeighbors.push(neighborKey);
+      }
+
+      if (livingNeighbors.length === 2 || livingNeighbors.length === 3) {
+        actCell.incAge();
+        nextGenCells.push(actCell);
+      }
+
+      for (const deadNeighborKey of deadNeighbors) {
+        deadNeighborKeysOfSoFarLivingCells.add(deadNeighborKey);
+      }
+    }
+
+    // check if dead cells awake
+    for (const key of deadNeighborKeysOfSoFarLivingCells) {
+      const split = key.split(',');
+      const deadCell = new Cell(parseInt(split[0]), parseInt(split[1]));
+      let livingNeighbors = 0;
+      for (const neighborKey of deadCell.getNeighborKeys()) {
+        if (this.livingCells.has(neighborKey))
+          livingNeighbors++;
+        if (livingNeighbors > 3) break;
+      }
+      if (livingNeighbors === 3)
+        nextGenCells.push(deadCell);
+    }
+
+    this.livingCells.clear();
+    for (const nextGenCell of nextGenCells) {
+      this.livingCells.set(nextGenCell.getKey(), nextGenCell);
+    }
+
+    this.calcMinMax();
+  }
+
+  draw(ctx) {
+    for (const cell of this.livingCells.values()) {
+      cell.draw(ctx);
+    }
+  }
+}
+
+class Cell {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.age = 1;
+  }
+
+  incAge() {
+    this.age++;
+  }
+
+  getKey() {
+    return this.x + ',' + this.y;
+  }
+
+  getNeighborKeys() {
+    const ownKey = this.getKey();
+    const neighbors = [];
+    for (let x = -1; x <= 1; x++) {
+      for (let y = -1; y <= 1; y++) {
+        const key = (this.x + x) + ',' + (this.y + y);
+        if (key !== ownKey)
+          neighbors.push(key);
+      }
+    }
+    return neighbors;
+  }
+
+  draw(ctx) {
+    const r = 0.5;
+    ctx.beginPath();
+    // ctx.arc(this.x, this.y, r, 0, Math.PI * 2);
+    ctx.rect(this.x - r, this.y - r, 2 * r, 2 * r);
+    ctx.fill();
+  }
+}
+
+export {Game};
---
+++ main.mjs
@@ -0,0 +1,59 @@
+import {Game} from "./game.mjs";
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext('2d');
+
+let worldWidth = canvas.width;
+let worldHeight = canvas.height;
+let worldWidth2 = worldWidth / 2;
+let worldHeight2 = worldHeight / 2;
+
+const updateWorldSettings = () => {
+  if (worldHeight !== window.innerHeight || worldWidth !== window.innerWidth) {
+    worldWidth = window.innerWidth;
+    worldHeight = window.innerHeight;
+    worldWidth2 = worldWidth / 2;
+    worldHeight2 = worldHeight / 2;
+    canvas.width = worldWidth;
+    canvas.height = worldHeight;
+  }
+};
+
+updateWorldSettings();
+
+const game = new Game();
+game.fillRandom();
+
+const stepTime = 100;
+let lastStep = Date.now();
+
+const update = () => {
+  const now = Date.now();
+  if (now > lastStep + stepTime) {
+    game.step();
+    lastStep = now;
+  }
+
+  ctx.clearRect(0, 0, worldWidth, worldHeight);
+
+  ctx.save();
+
+  const xScale = worldWidth / (4 + Math.max(Math.abs(game.xMax), Math.abs(game.xMin)) * 2);
+  const yScale = worldHeight / (4 + Math.max(Math.abs(game.yMax), Math.abs(game.yMin)) * 2);
+  const scale = Math.min(xScale, yScale);
+  ctx.translate(worldWidth2 - 1.5, worldHeight2 - 1.5);
+  ctx.scale(scale, scale);
+  game.draw(ctx);
+
+  ctx.restore();
+  updateWorldSettings();
+
+
+  requestAnimationFrame(update);
+}
+
+update();
+
+document.body.addEventListener('click', () => {
+  game.step();
+});
